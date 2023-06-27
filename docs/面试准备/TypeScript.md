6d7a27acf5c33c647fe004085616dae9055c1b4d
Ye <3011852376@qq.com>
2023-06-27 17:27:16 +0800

    update

--- docs/面试准备/TypeScript.md
+++ docs/面试准备/TypeScript.md
@@ -2,8 +2,17 @@
 
 ## interface 和 type 的区别
 
-- `interface`可以重复声明（属性合并），`type`不可以
-- 继承方式不同，`interface`使用`extends`继承，`type`使用 &（即交叉类型）
+##### 相同点
+
+- 都是用来定义对象或者函数的类型
+- 都可以继承，方式不同，`interface`使用`extends`继承，`type`使用 &（即交叉类型），可以互相继承
+
+##### 不同点
+
+- `interface`可以重复声明（属性合并），`type`会报重复定义错误
+- `type`可以定义类型别名，例如`type myString = string`
+- `type`可以使用操作符，例如`typeof`、`keyof`
+- `type`可以使用联合类型或者交叉类型
 
 ## 常见工具类型
 
@@ -15,4 +24,8 @@
 - `Extract`：交集
 - `Exclude`：差集
 
-## 泛型
+## any 和 unknown 的区别
+
+- 区别：`any` 不强制进行类型检查，可以存储任何类型的值也可以赋值给任何类型的变量；`unknown` 可以存储任何类型的值但是只能赋值给 `any` 和 `unknown` 类型的变量
+- 使用场景：
+  - unknown 一般用作函数参数，用来接收任意类型的变量实参
--- docs/面试准备/demo.js
+++ docs/面试准备/demo.js
@@ -1,15 +1,13 @@
-const flatten = (arr) => {
-  let result = [];
-
-  arr.forEach(item => {
-    if (Array.isArray(item)) {
-      result = result.concat(flatten(item));
-    } else {
-      result.push(item);
-    }
-  });
-
-  return result;
-}
-const arr = [1, [2, [3, 4, 5]]];
-console.log([].concat(...[1, [2, [3,4,5]]]));
+var querystring = require('querystring');
+var http = require('http');
+var server = http.createServer();
+server.on('request', function(req, res) {
+  var params = querystring.parse(req.url.split('?')[1]);
+  var fn = params.callback;
+  // jsonp返回设置
+  res.writeHead(200, { 'Content-Type': 'text/javascript' });
+  res.write(`${fn}(${JSON.stringify(params)})`);
+  res.end();
+})
+server.listen('8080');
+console.log('Server is running at port 8080...');
